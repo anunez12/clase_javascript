0268a7f1f2b139d22f7c2262ab6a27e680af742f
anunez12 <albin.ernesto78@hotmail.com>
2021-05-01 10:37:51 -0500

    Agregado alert y prueba live server

---
+++ index.html/script.js
@@ -0,0 +1,15 @@
+//saludo 
+alert('hola DIOS los bendiga y feliz dia del trabajador');
+//comentatio de linea
+/*comentario de mas de una linea
+*/
+
+//sentencias
+alert('Hola mundo'); alert('vinicius pa cuando');
+
+alert(3+2+5)
+
+//variables
+let mensaje;
+mensaje= "Cristobal soria";
+console.log(mensaje)
